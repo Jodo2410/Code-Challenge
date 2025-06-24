322e446c55e2a8f5849b912af89fe99ae10fca16
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-24 22:32:56 +0200

    Add csharp challenge: hAL

---
+++ challenges/hal-mcazfk9x/starter.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Hal
+{
+    /// <summary>
+    /// WADAWDAWDAWDAWDAWDAWDAWDAWD
+    /// </summary>
+    public static object hal()
+    {
+        // TODO: Implementiere deine Lösung hier
+        throw new NotImplementedException();
+    }
+}
