d6f235bda461efd1efc01739308779a9e8842253
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-24 23:47:30 +0200

    ✅ Add approved challenge: Test

---
+++ challenges/test-mcb23gkh/starter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stdexcept>
+
+/**
+ * Wwwwwwwwwwwwwwwwwww
+ */
+class Test {
+public:
+    static auto test() {
+        // TODO: Implementiere deine Lösung hier
+        throw std::runtime_error("Noch nicht implementiert");
+    }
+};
