3002c3de3aedf9189f2de2fc67c03171209d67dd
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-24 17:18:50 +0200

    Add challenge: Array Sum Calculator

---
+++ challenges/array-sum-calculator-mcao7nh2/starter.cs
@@ -0,0 +1,58 @@
+using System;
+
+public class ArraySumCalculator
+{
+    /// <summary>
+    /// Berechnet die Gesamtsumme aller Zahlen im Array
+    /// </summary>
+    /// <param name="numbers">Array von Ganzzahlen</param>
+    /// <returns>Summe aller Zahlen</returns>
+    public static int TotalSum(int[] numbers)
+    {
+        // TODO: Implementiere die Gesamtsumme
+        // Tipp: Verwende eine foreach-Schleife
+        
+        throw new NotImplementedException("TotalSum noch nicht implementiert");
+    }
+
+    /// <summary>
+    /// Berechnet die Summe aller geraden Zahlen im Array
+    /// </summary>
+    /// <param name="numbers">Array von Ganzzahlen</param>
+    /// <returns>Summe aller geraden Zahlen</returns>
+    public static int SumEvenNumbers(int[] numbers)
+    {
+        // TODO: Implementiere die Summe gerader Zahlen
+        // Tipp: Eine Zahl ist gerade wenn number % 2 == 0
+        
+        throw new NotImplementedException("SumEvenNumbers noch nicht implementiert");
+    }
+
+    /// <summary>
+    /// Berechnet die Summe aller ungeraden Zahlen im Array
+    /// </summary>
+    /// <param name="numbers">Array von Ganzzahlen</param>
+    /// <returns>Summe aller ungeraden Zahlen</returns>
+    public static int SumOddNumbers(int[] numbers)
+    {
+        // TODO: Implementiere die Summe ungerader Zahlen
+        // Tipp: Eine Zahl ist ungerade wenn number % 2 != 0
+        
+        throw new NotImplementedException("SumOddNumbers noch nicht implementiert");
+    }
+
+    /// <summary>
+    /// Berechnet die Summe aller Zahlen zwischen min und max (inklusive)
+    /// </summary>
+    /// <param name="numbers">Array von Ganzzahlen</param>
+    /// <param name="min">Minimaler Wert (inklusive)</param>
+    /// <param name="max">Maximaler Wert (inklusive)</param>
+    /// <returns>Summe aller Zahlen im angegebenen Bereich</returns>
+    public static int SumInRange(int[] numbers, int min, int max)
+    {
+        // TODO: Implementiere die Summe von Zahlen in einem Bereich
+        // Tipp: Prüfe ob number >= min && number <= max
+        
+        throw new NotImplementedException("SumInRange noch nicht implementiert");
+    }
+}
