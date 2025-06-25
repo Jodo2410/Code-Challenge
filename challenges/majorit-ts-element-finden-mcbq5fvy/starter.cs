4b5fe16d56d75f815991cd4f266194928d67f10d
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-25 11:00:53 +0200

    🎯 Add new csharp challenge: Majoritäts-Element finden
    - Language: csharp
    - Difficulty: medium
    - Author: Jodo2410
    - Tags: arrays, mehrheitswahl, boyer-moore, algorithmus, csharp, medium
    
    ⏳ Waiting for admin review and approval.

---
+++ challenges/majorit-ts-element-finden-mcbq5fvy/starter.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public static class Solution {
+    public static int FindMajorityElement(int[] nums) {
+        // TODO: Implementiere die Lösung
+        return -1;
+    }
+}
