1566e1f7c3e3b4eca927cde0c703445fd3c3b3e6
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-25 12:27:12 +0200

    🎯 Add new csharp challenge: Regulärer Ausdrucksabgleich
    - Language: csharp
    - Difficulty: hard
    - Author: Jodo2410
    - Tags: dynamic-programming, string-matching, regex, algorithms, csharp, hard
    
    ⏳ Waiting for admin review and approval.

---
+++ challenges/regul-rer-ausdrucksabgleich-mcbt8fxs/starter.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class RegexMatcher
+{
+    /// <summary>
+    /// Prüft, ob ein String mit einem regulären Ausdruck übereinstimmt.
+    /// </summary>
+    /// <param name="s">Der zu prüfende String</param>
+    /// <param name="p">Das reguläre Ausdrucksmuster</param>
+    /// <returns>True, wenn der String mit dem Muster übereinstimmt, sonst False</returns>
+    public static bool IsMatch(string s, string p)
+    {
+        // TODO: Implementiere deine Lösung hier
+        throw new NotImplementedException();
+    }
+}
