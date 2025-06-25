89d41edc7c1c4f53dadc11def1f07b1da426cec2
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-25 12:25:54 +0200

    🎯 Add new csharp challenge: Palindromische Teilstrings zählen
    - Language: csharp
    - Difficulty: easy
    - Author: Jodo2410
    - Tags: strings, dynamic-programming, palindrome, algorithms, csharp, easy
    
    ⏳ Waiting for admin review and approval.

---
+++ challenges/palindromische-teilstrings-z-hlen-mcbt6rlu/starter.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class PalindromeCounter
+{
+    /// <summary>
+    /// Zählt die Anzahl der palindromischen Teilstrings in einem gegebenen String.
+    /// </summary>
+    /// <param name="s">Der zu untersuchende String</param>
+    /// <returns>Die Anzahl der palindromischen Teilstrings</returns>
+    public static int CountPalindromicSubstrings(string s)
+    {
+        // TODO: Implementiere deine Lösung hier
+        throw new NotImplementedException();
+    }
+}
