aaf847ebbc447cf5b90d4a6dafa81719d85991e3
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-25 14:36:30 +0200

    🎯 Add new csharp challenge: Zähle gültige Subarrays mit gleicher Anzahl von 0 und 1
    - Language: csharp
    - Difficulty: medium
    - Author: Jodo2410
    - Tags: arrays, hashmap, prefixsum, algorithmen, csharp, medium
    
    ⏳ Waiting for admin review and approval.

---
+++ challenges/z-hle-g-ltige-subarrays-mit-gleicher-anzahl-von-0-und-1-mcbxuq8p/starter.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public static class Solution {
+    public static int CountBalancedSubarrays(int[] nums) {
+        // Deine Lösung hier
+        return 0;
+    }
+}
