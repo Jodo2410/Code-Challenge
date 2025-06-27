c5cb4d5f74d548cf642cd8f8a16f3c6437ba9ce9
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-27 10:03:44 +0200

    🎯 Add new csharp challenge: Duplikate aus Array entfernen
    - Language: csharp
    - Difficulty: easy
    - Author: Jodo2410
    - Tags: csharp, arrays, listen, duplikate, filter, easy
    
    ⏳ Waiting for admin review and approval.

---
+++ challenges/duplikate-aus-array-entfernen-mceiznes/starter.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class ArrayUtils
+{
+    public static int[] RemoveDuplicates(int[] input)
+    {
+        // TODO: Entferne doppelte Werte aus dem Array
+        return new int[0];
+    }
+}
