511eb3cc6b54ae1bbf55ec3d4840cfea13725eb4
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-27 16:01:13 +0200

    Create Challenge.cs

---
+++ templates/csharp/Challenge.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ChallengeTemplate
+{
+    /// <summary>
+    /// Implementiere deine Challenge-Lösung hier
+    /// </summary>
+    public class Challenge
+    {
+        /// <summary>
+        /// TODO: Beschreibe deine Challenge hier
+        /// </summary>
+        public static int Solve(int[] input)
+        {
+            // TODO: Implementiere deine Lösung hier
+            throw new NotImplementedException("Implementiere deine Challenge-Lösung");
+        }
+    }
+}
