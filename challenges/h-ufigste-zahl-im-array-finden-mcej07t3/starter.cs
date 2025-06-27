4e831266a74274192240938e5d402b60793b035b
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-27 10:04:10 +0200

    🎯 Add new csharp challenge: Häufigste Zahl im Array finden
    - Language: csharp
    - Difficulty: easy
    - Author: Jodo2410
    - Tags: csharp, arrays, häufigkeit, modus, statistik, easy
    
    ⏳ Waiting for admin review and approval.

---
+++ challenges/h-ufigste-zahl-im-array-finden-mcej07t3/starter.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class FrequencyAnalyzer
+{
+    public static int MostFrequent(int[] input)
+    {
+        // TODO: Finde die Zahl mit der höchsten Häufigkeit
+        return 0;
+    }
+}
