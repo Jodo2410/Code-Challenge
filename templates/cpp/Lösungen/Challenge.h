2d6ff835ca0f0d66087211346adfa941cdcea3f5
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-30 09:18:03 +0200

    Update Challenge.h

--- templates/cpp/Lösungen/Challenge.h
+++ templates/cpp/Lösungen/Challenge.h
@@ -1,1 +1,44 @@
+#ifndef CHALLENGE_H
+#define CHALLENGE_H
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <utility>
+#include <cmath>
+#include <climits>
+
+/**
+ * {{CHALLENGE_TITLE}} - C++ Template-Challenge
+ * 
+ * Beschreibung: {{CHALLENGE_DESCRIPTION}}
+ * Schwierigkeit: {{DIFFICULTY}}
+ * Sprache: C++17
+ * 
+ * Template-Version: {{TEMPLATE_VERSION}}
+ * Erstellt: {{CREATED_DATE}}
+ * Autor: {{AUTHOR}}
+ */
+
+class Challenge {
+public:
+    /**
+     * Haupt-Challenge Funktion
+     * TODO: Implementiere deine Lösung hier
+     * 
+     * @param input - Eingabe Parameter (anpassen je nach Challenge)
+     * @return Ergebnis (Typ anpassen je nach Challenge)
+     */
+    static int solve(const std::vector<int>& input);
+
+    
+private:
+    // --
+};
+
+#endif // CHALLENGE_H
