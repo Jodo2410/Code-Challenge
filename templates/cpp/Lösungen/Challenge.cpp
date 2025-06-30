776d39032c669f8642ee124cf3e5f70c3a2b199e
Jodo2410 <91252292+Jodo2410@users.noreply.github.com>
2025-06-30 09:17:53 +0200

    Update Challenge.cpp

--- templates/cpp/Lösungen/Challenge.cpp
+++ templates/cpp/Lösungen/Challenge.cpp
@@ -1,1 +1,18 @@
+#include "Challenge.h"
+#include <numeric>
+#include <stdexcept>
 
+/**
+ * Challenge Implementation
+ * Template-Version: 2.0
+ */
+
+/**
+ * Haupt-Challenge Funktion
+ * TODO: Implementiere deine Lösung hier
+ */
+int Challenge::solve(const std::vector<int>& input) {
+
+    // Hier die lösung implementieren
+
+}
